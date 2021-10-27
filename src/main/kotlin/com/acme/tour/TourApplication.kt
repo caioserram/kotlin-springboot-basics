73fee310fca47ea10396be1c50a9c13a16de662c
Caio Serra <caio_mello@id.uff.br>
2021-10-27 11:15:46 -0300

    adiciona bean de promocoes mock na main application

--- src/main/kotlin/com/acme/tour/TourApplication.kt
+++ src/main/kotlin/com/acme/tour/TourApplication.kt
@@ -1,10 +1,25 @@
 package com.acme.tour
 
+import com.acme.tour.model.Promocao
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.util.concurrent.ConcurrentHashMap
 
 @SpringBootApplication
-class TourApplication
+class TourApplication {
+	companion object {
+		val initialPromocoes = arrayOf(
+			Promocao(1, "Maravilhosa viagem a Cancun", "Cancun", true, 7, 4999.99),
+			Promocao(2,"Viagem radical com rapel e escalada", "Nova Zelandia", false, 12, 12000.0),
+			Promocao(3, "Viagem espiritual", "Thailandia", false, 17, 15000.0),
+			Promocao(4, "Viagem com a familia", "Gramado", false, 5, 3500.33)
+		)
+	}
+
+	@Bean
+	fun promocoes() = ConcurrentHashMap<Long, Promocao> ( initialPromocoes.associateBy ( Promocao::id )  )
+}
 
 fun main(args: Array<String>) {
 	runApplication<TourApplication>(*args)
