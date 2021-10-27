8e733bd4563d93fe5887c7b4325bb5b5ef3f5708
Caio Serra <caio_mello@id.uff.br>
2021-10-27 13:14:24 -0300

    cria interface de promocao service

---
+++ curso1-intro-kotlin-springboot/src/main/kotlin/com/acme/tour/service/PromocaoService.kt
@@ -0,0 +1,11 @@
+package com.acme.tour.service
+
+import com.acme.tour.model.Promocao
+
+interface PromocaoService {
+    fun create(promocao:Promocao)
+    fun getById(id: Long): Promocao?
+    fun update(id:Long, promocao: Promocao)
+    fun delete(id:Long )
+    fun searchByLocal(local: String): List<Promocao>
+}
