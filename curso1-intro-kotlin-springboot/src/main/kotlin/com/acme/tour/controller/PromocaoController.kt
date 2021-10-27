ddb4d8b6de485a80aa8bb8d70d94ceb75fb9d349
Caio Serra <caio_mello@id.uff.br>
2021-10-27 18:24:09 -0300

    promocoes getById adiciona verificacao para retorno 404

--- curso1-intro-kotlin-springboot/src/main/kotlin/com/acme/tour/controller/PromocaoController.kt
+++ curso1-intro-kotlin-springboot/src/main/kotlin/com/acme/tour/controller/PromocaoController.kt
@@ -3,6 +3,8 @@ package com.acme.tour.controller
 import com.acme.tour.model.Promocao
 import com.acme.tour.service.PromocaoService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.concurrent.ConcurrentHashMap
 
@@ -18,7 +20,12 @@ class PromocaoController {
         this.promocaoService.searchByLocal(localFilter)
 
     @GetMapping("/{id}")
-    fun getById(@PathVariable id: Long) = this.promocaoService.getById(id)
+    fun getById(@PathVariable id: Long): ResponseEntity<Promocao?> {
+        val promocao = this.promocaoService.getById(id)
+        val status = if (promocao == null) HttpStatus.NOT_FOUND else HttpStatus.OK
+
+        return ResponseEntity(promocao,status)
+    }
 
     @PostMapping()
     fun create(@RequestBody promocao: Promocao) {
