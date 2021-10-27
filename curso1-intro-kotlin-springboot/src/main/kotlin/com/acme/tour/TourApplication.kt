5cf34734c8387ad4aeb4c1a89d264232a5fe2ee6
Caio Serra <caio_mello@id.uff.br>
2021-10-27 13:14:53 -0300

    remove codigo de promocoes do application

--- curso1-intro-kotlin-springboot/src/main/kotlin/com/acme/tour/TourApplication.kt
+++ curso1-intro-kotlin-springboot/src/main/kotlin/com/acme/tour/TourApplication.kt
@@ -7,19 +7,7 @@ import org.springframework.context.annotation.Bean
 import java.util.concurrent.ConcurrentHashMap
 
 @SpringBootApplication
-class TourApplication {
-	companion object {
-		val initialPromocoes = arrayOf(
-			Promocao(1, "Maravilhosa viagem a Cancun", "Cancun", true, 7, 4999.99),
-			Promocao(2,"Viagem radical com rapel e escalada", "Nova Zelandia", false, 12, 12000.0),
-			Promocao(3, "Viagem espiritual", "Thailandia", false, 17, 15000.0),
-			Promocao(4, "Viagem com a familia", "Gramado", false, 5, 3500.33)
-		)
-	}
-
-	@Bean
-	fun promocoes() = ConcurrentHashMap<Long, Promocao> ( initialPromocoes.associateBy ( Promocao::id )  )
-}
+class TourApplication
 
 fun main(args: Array<String>) {
 	runApplication<TourApplication>(*args)
