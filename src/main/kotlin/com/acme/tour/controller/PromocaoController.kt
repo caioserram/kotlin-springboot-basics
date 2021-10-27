d0df0bb6e579024492c5ee58e25a5932308915aa
Caio Serra <caio_mello@id.uff.br>
2021-10-27 11:16:18 -0300

    adiciona método get promocoes utilizando o hashmap

--- src/main/kotlin/com/acme/tour/controller/PromocaoController.kt
+++ src/main/kotlin/com/acme/tour/controller/PromocaoController.kt
@@ -1,14 +1,23 @@
 package com.acme.tour.controller
 
+import com.acme.tour.model.Promocao
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.ConcurrentHashMap
 
 @RestController
-class Promocao {
+class PromocaoController {
 
-    @RequestMapping(value=["/promocoes"], method = [RequestMethod.GET])
+    @Autowired
+    lateinit var initialPromocoes: ConcurrentHashMap<Long, Promocao>
+
+    @RequestMapping(value=["/sayHello"], method = [RequestMethod.GET])
     fun sayHello(): String {
         return "Hello World"
     }
+
+    @RequestMapping(value = ["/promocoes"], method = [RequestMethod.GET])
+    fun getPromocao() = this.initialPromocoes[2]
 }
