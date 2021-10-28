9c8755bb5340028fabf9a35bec472a7209c084cc
Caio Serra <caio_mello@id.uff.br>
2021-10-28 20:17:50 -0300

    model promocao adiciona anotacoes entity

--- curso1-intro-kotlin-springboot/src/main/kotlin/com/acme/tour/model/Promocao.kt
+++ curso1-intro-kotlin-springboot/src/main/kotlin/com/acme/tour/model/Promocao.kt
@@ -1,10 +1,19 @@
 package com.acme.tour.model
 
+import org.intellij.lang.annotations.Identifier
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
 data class Promocao(
-    val id: Long,
-    val descricao: String,
-    val local: String,
-    val isAllInclusive: Boolean,
-    val qtdDias: Int,
-    val preco: Double
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 1,
+    val descricao: String = "",
+    val local: String = "",
+    val isAllInclusive: Boolean = false,
+    val qtdDias: Int = 0,
+    val preco: Double = 0.0
 )
