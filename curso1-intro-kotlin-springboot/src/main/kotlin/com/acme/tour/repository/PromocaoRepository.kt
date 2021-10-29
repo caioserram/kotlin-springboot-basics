6b4949eb777f42d8e851f09525e5e00ea4418bd1
Caio Serra <caio_mello@id.uff.br>
2021-10-28 21:39:26 -0300

    promocao adiciona funcao de atualizar preco por local

--- curso1-intro-kotlin-springboot/src/main/kotlin/com/acme/tour/repository/PromocaoRepository.kt
+++ curso1-intro-kotlin-springboot/src/main/kotlin/com/acme/tour/repository/PromocaoRepository.kt
@@ -1,10 +1,12 @@
 package com.acme.tour.repository
 
 import com.acme.tour.model.Promocao
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
 @Repository
 interface PromocaoRepository: PagingAndSortingRepository<Promocao, Long> {
@@ -14,4 +16,8 @@ interface PromocaoRepository: PagingAndSortingRepository<Promocao, Long> {
 
     @Query("select p from Promocao p where p.local in :localNames")
     fun findByLocalInList(@Param("localNames") localNames: String)
+
+    @Query("update Promocao p set p.preco = :valor where p.local = :local")
+    @Transactional @Modifying
+    fun updatePrecoByLocal(@Param("valor") valor: Double, @Param("local") local: String)
 }
