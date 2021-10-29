03de2ebca46cd33982e00d136f20372f18f943e0
Caio Serra <caio_mello@id.uff.br>
2021-10-28 21:14:21 -0300

    data sql configura aplicação para incluir no banco dados iniciais

---
+++ curso1-intro-kotlin-springboot/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO promocao
+(descricao,is_all_inclusive,local,preco,qtd_dias) VALUES
+('Viagem de familia',0,'Campos do Jordão',600.0,5),
+('Viagem de Radical',0,'Nova Zelandia',15000.0,15),
+('Viagem de Religiosa',0,'Bali',9000.0,12),
+('Viagem para ver animais selvagens',0,'Cidade do Cabo',8700.0,8);
